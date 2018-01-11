e0b78032c485e83b099bdda313f0bc54eef3fd3f
mountbicepius <mountbicepius@github.com>
2018-01-11 21:24:42 +0530

    help me

--- app/src/main/java/com/anirudh/echeval/perfect/MainActivity.java
+++ app/src/main/java/com/anirudh/echeval/perfect/MainActivity.java
@@ -1,13 +1,52 @@
 package com.anirudh.echeval.perfect;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img =findViewById(R.id.imgLogo);
+        setAlphaAnimation(img);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,HomeActivity.class));
+
+                finish();
+            }
+        }, 2*1000);
+    }
+    public static void setAlphaAnimation(View v) {
+        ObjectAnimator fadeOut = ObjectAnimator.ofFloat(v, "alpha",  1f, .3f);
+        fadeOut.setDuration(2000);
+        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(v, "alpha", .3f, 1f);
+        fadeIn.setDuration(2000);
+
+        final AnimatorSet mAnimationSet = new AnimatorSet();
+
+        mAnimationSet.play(fadeOut).after(fadeIn);
+
+        mAnimationSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                mAnimationSet.start();
+            }
+        });
+        mAnimationSet.start();
     }
 }
